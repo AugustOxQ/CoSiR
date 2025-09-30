Fix CCdataset loading memory leakage issue

src/dataset/conceptual_datamodule.py
@@ -1,3 +1,4 @@
+import os
 from PIL import ImageFile
 from torch.utils.data import Dataset
 from datasets import load_dataset, config
@@ -12,20 +13,38 @@ class FeatureExtractionConceptualDataset(Dataset):
         config.HF_DATASETS_CACHE = annotation_path
 
         ratio = int(min(max(ratio, 0.01), 1) * 100)
+
+        # Pre-load length of annotations so we can use streaming
+        annotation_length_path = os.path.join(annotation_path, "annotation_length.txt")
+        self.annotation_length = -1
+        if os.path.exists(annotation_length_path):
+            self.annotation_length = int(open(annotation_length_path).read())
+            streaming = True
+        else:
+            streaming = False
+
+        # Load annotations
         self.annotations = load_dataset(
             image_path,
             cache_dir=annotation_path,
             split=f"train[:{ratio}%]",
-            # streaming=True, # But here we need to get sample ids, so we can't use streaming
+            streaming=streaming,  # But here we need to get sample ids, so we can't use streaming
         )
+
+        # If we didn't pre-load the length of annotations, we need to load the annotations to get the length
+        if self.annotation_length == -1:
+            self.annotation_length = len(self.annotations)
+            open(annotation_length_path, "w").write(str(self.annotation_length))
+
+        # Assign unique numeric IDs to each sample
+        self.sample_ids = {
+            i: idx for idx, i in enumerate(range(self.annotation_length))
+        }
 
         self.processor = processor
 
-        # Assign unique numeric IDs to each sample
-        self.sample_ids = {i: idx for idx, i in enumerate(range(len(self.annotations)))}
-
     def __len__(self):
-        return len(self.annotations)
+        return self.annotation_length
 
     def __getitem__(self, idx: int) -> tuple:
         """Get the processed image and textual annotation for a given index."""