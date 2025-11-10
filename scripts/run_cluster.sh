da0ddc276f6d4ac205f40a8bc935e19d0dd50272
Wangyuan Ding <w.ding@uva.nl>
2025-11-10 23:29:37 +0100

    Add no-param network

--- scripts/run_cluster.sh
+++ scripts/run_cluster.sh
@@ -21,21 +21,21 @@ ENTRY="main_cosir.py"
 
 echo "Experiment 1, from 1e-6 to 1e-6"
 
-CUDA_VISIBLE_DEVICES=0 python main_cosir.py dataset=cc3m_cluster train.lr=1e-6 > exp1.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python main_cosir.py dataset=cc3m_cluster optimizer.lr=1e-6 > exp1.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python main_cosir.py dataset=cc3m_cluster train.lr=5e-6 > exp2.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python main_cosir.py dataset=cc3m_cluster optimizer.lr=5e-6 > exp2.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python main_cosir.py dataset=cc3m_cluster train.lr=1e-5 > exp3.log 2>&1 &
+CUDA_VISIBLE_DEVICES=2 python main_cosir.py dataset=cc3m_cluster optimizer.lr=1e-5 > exp3.log 2>&1 &
 
 wait
 
 echo "Experiment 2, from 5e-5 to 5e-4"
 
-CUDA_VISIBLE_DEVICES=0 python main_cosir.py dataset=cc3m_cluster train.lr=5e-5 > exp4.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python main_cosir.py dataset=cc3m_cluster optimizer.lr=5e-5 > exp4.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1 python main_cosir.py dataset=cc3m_cluster train.lr=1e-4 > exp5.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python main_cosir.py dataset=cc3m_cluster optimizer.lr=1e-4 > exp5.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2 python main_cosir.py dataset=cc3m_cluster train.lr=5e-4 > exp6.log 2>&1 &
+CUDA_VISIBLE_DEVICES=2 python main_cosir.py dataset=cc3m_cluster optimizer.lr=5e-4 > exp6.log 2>&1 &
 
 wait
 
