1ffdbead8f30f4abe894d979fb2d1280dc164dee
Wangyuan Ding <w.ding@uva.nl>
2026-06-02 23:00:17 +0200

    force multirun to have group name

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -30,7 +30,7 @@ python main_cosir.py -m \
   loss.lambda_mixup=1 \
   loss.lambda_delta=0.1 \
   model=clip_base \
-  experiment.results_dir="res/CoSiR_Experiment_phase2/impressions" \
+  experiment.results_dir="res/CoSiR_Experiment_phase2_5/impressions" \
   model.num_layers=6 \
   model.embedding_dim=16 \
   "optimizer.lr=1e-5, 1e-4, 1e-3" \
@@ -39,3 +39,4 @@ python main_cosir.py -m \
   train.normalize=False \
   train.imgtxt_factor=1 \
   train.initialization_strategy=imgtxt \
+  wandb.group="phase2.5 adding VICReg-style loss" \
